8a84d2b16816a2cb7cbc7669daec58e1e1afd6cf
Joaquim Leal <1090956@isep.ipp.pt>
2019-01-04 23:38:47 +0000

    Unit Test: Dto: Revert last update Controller: Add test suite

--- test/test.js
+++ test/test.js
@@ -1,139 +1,121 @@
 var Assert = require('assert');
+
+const Controller = require('../controllers/orderController');
 const Service = require('../services/orderService');
 const Item = require('../models/item');
 const Order = require('../models/order');
-const Customer = require('../models/customer')
-const Address = require('../models/address')
+const Customer = require('../models/customer');
+const Address = require('../models/address');
 const { State } = require('../models/order');
+const OrderDto = require('../Dtos/OrderDto');
 
-describe('Order Suite 1', function () {
-    var street1= '1st avenue';
-    var door1=1;
-    var postalCodeCity1= 4100;
-    var postalCodeStreet1= 10;
-    var newAddress;
-    var newCustomer;
-    var newProduct;
-    var totalPrice;
-    var newOrder;
-    it('Test 1: createAddress', function (){
-        var jSonAddress=
-        {
-            street:street1,
-            door:door1,
-            postalCodeCity:postalCodeCity1,
-            postalCodeStreet:postalCodeStreet1
-        }
-        newAddress=Service.createAddress(jSonAddress);
-        Assert.equal(jSonAddress.street, newAddress.street);
-        Assert.equal(jSonAddress.door, newAddress.door);
-        Assert.equal(jSonAddress.postalCodeCity, newAddress.postalCodeCity);
-        Assert.equal(jSonAddress.postalCodeStreet, newAddress.postalCodeStreet);
-    });
-    it('Test 2: createCustomer', function () {
-        var name1= 'Customer 1';
-        var email1="first@customer,com";
-        var phone1=919191919;
-        var address1=
-        {
-            street:street1,
-            door:door1,
-            postalCodeCity:postalCodeCity1,
-            postalCodeStreet:postalCodeStreet1
-        }
-        var customer=
-        {
-            name:name1,
-            email:email1,
-            phone:phone1,
-            address:address1
-        }
+describe('Suite 1: Controller', function () {
 
-        newCustomer=Service.createCustomer(customer);
-
-        Assert.equal(customer.name, newCustomer.name);
-        Assert.equal(customer.email, newCustomer.email);
-        Assert.equal(customer.phone, newCustomer.phone);
-    });
-    it('Test 3: createNewProduct', function () {
-        var product=
-        {
-            productId:1,
-            material:'Wood',
-            category:'Closet',
-            finish: 'None',
-            name: 'Product 1',
-            price:30,
-            width:2.0,
-            height:1.0,
-            depth:0.5,
-            children: []
-        }
-
-        newProduct=Service.createNewProduct(product2);
-        Assert.equal(product.productId, newProduct.productId);
-        Assert.equal(product.material, newProduct.material);
-        Assert.equal(product.category, newProduct.category);
-        Assert.equal(product.finish, newProduct.finish);
-        Assert.equal(product.name, newProduct.name);
-        Assert.equal(product.price, newProduct.price);
-        Assert.equal(product.width, newProduct.width);
-        Assert.equal(product.height, newProduct.height);
-        Assert.equal(product.depth, newProduct.depth);
-        Assert.equal(product.children, newProduct.children);
+    it('Test 1: createOrder', function () {
+        // Arrange
+        order = Controller.create_order();
+        item = new Item();
+        item = { name : 'Product Test1' };
+        order = { 
+            customer : 'Customer Test1',
+            address : 'Address Test1',
+            state : 1,
+            items : [item],
+            totalPrice : 30
+        };
+        // Act
+        var resultCustomer = "Customer Test1";
+        var resultAddress = "Address Test1";
+        var resultState = 1;
+        var resultItem = "Product Test1"
+        var resultTotalPrice = 30;
+        // Assert
+        Assert.equal(order.customer, resultCustomer);
+        Assert.equal(order.address, resultAddress);
+        Assert.equal(order.state, resultState);
+        Assert.equal(order.items[0].name, resultItem);
+        Assert.equal(order.totalPrice, resultTotalPrice);
     });
 
-    it('Test 4: CreateNewOrder', function () {
-        var order=
-        {
-            customer:newCustomer,
-            address:newAddress,
-            totalPrice:totalPrice,
-            items: [newProduct]
-        }
-        newOrder=Service.createNewOrder(order);
+});
 
-        Assert.equal(order.customer, newOrder.customer);
-        Assert.equal(order.address, newOrder.address);
-        Assert.equal(order.totalPrice, newOrder.totalPrice);
-        Assert.equal(order.items, newOrder.items);
-        Assert.equal(order.state, State.SUBMETIDA);
+
+describe('Suite 2: Dtos', function () {
+
+    it('Test 1: Order.customer', function () {           
+        // Arrange
+        const order = new OrderDto();
+        order.customer = "Customer Test1";
+        // Act
+        const resultCustomer = "Customer Test1";
+        // Assert
+        Assert.equal(order.customer, resultCustomer);
+    });
+    
+    it('Test 2: Order.address', function () {
+        // Arrange
+        const order = new OrderDto();
+        order.address = "Address Test1";
+        // Act
+        const resultAddress = "Address Test1";
+        // Assert
+        Assert.equal(order.address, resultAddress);
+    });
+    
+    it('Test 3: Order.Items', function () {
+        // Arrange
+        const order = new OrderDto();
+        const item = new Item();
+        order.Items = [{
+            productId : 1,
+            material : 'Wood',
+            category : 'Closet',
+            finish : 'None',
+            name : 'Product 1',
+            price : 30,
+            width : 2.0,
+            height : 1.0,
+            depth : 0.5,
+            children : []
+        }];
+        // Act
+        const resultMaterial = "Wood";
+        const resultCategory = "Closet";
+        const resultFinish = "None";
+        const resultName = "Product 1";
+        const resultPrice = 30;
+        const resultWidth = 2.0;
+        const resultHeight = 1.0;
+        const resultDepth = 0.5;
+        // Assert
+        Assert.equal(order.Items[0].material, resultMaterial);
+        Assert.equal(order.Items[0].category, resultCategory);
+        Assert.equal(order.Items[0].finish, resultFinish);
+        Assert.equal(order.Items[0].name, resultName);
+        Assert.equal(order.Items[0].price, resultPrice);
+        Assert.equal(order.Items[0].width, resultWidth);
+        Assert.equal(order.Items[0].height, resultHeight);
+        Assert.equal(order.Items[0].depth, resultDepth);
+    });
+    
+    it('Test 4: Order.totalPrice', function () {
+        // Arrange
+        const order = new OrderDto();
+        order.totalPrice = 100;
+        // Act
+        const resultTotalPrice = 100;
+        // Assert
+        Assert.equal(order.totalPrice, resultTotalPrice);
     });
 
-    it('Test 5: ProductFit', function (){
-        var productLeast=
-        {
-            productId:1,
-            material:'Wood',
-            category:'Closet',
-            finish: 'None',
-            name: 'Product 1',
-            price:30,
-            width:2.0,
-            height:1.0,
-            depth:0.5,
-            children: []
-        }
-        var productBiggest=
-        {
-            productId:1,
-            material:'Wood',
-            category:'Closet',
-            finish: 'None',
-            name: 'Product 1',
-            price:30,
-            width:3.0,
-            height:2.0,
-            depth:1.5,
-            children: []
-        }
-
-        var expResult=true;
-        var result=Service.productFit(productBiggest, productLeast);
-        Assert.equal(expResult, result);
-
-        expResult=false;
-        result=Service.productFit(productLeast, productBiggest);
-        Assert.equal(expResult, result);
+    it('Test 5: Order.state', function () {
+        // Arrange
+        const order = new OrderDto();
+        order.state = 1;
+        // Act
+        const resultState = 1;
+        // Assert
+        Assert.equal(order.state, resultState);
     });
+    
 });
