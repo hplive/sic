10c8df670cb4eda49df002f6a2a267b797306386
Ilidio Vatuva <1161431@isep.ipp.pt>
2019-01-08 19:59:20 +0000

    Order router changed.

--- routes/order.js
+++ routes/order.js
@@ -5,7 +5,7 @@ const OrderController = require('../controllers/orderController');
 router.post("/:user", OrderController.create_order);
 router.put("/:id", OrderController.set_state)
 router.get("/:id", OrderController.get_order);
-router.get("/:user", OrderController.get);
+router.get("/all/:user", OrderController.get);
 router.get("/:id/items", OrderController.get_orders_items);
 router.get("/:id/items/:itemId", OrderController.get_items_order);
 router.delete("/:id",OrderController.delete_order);
